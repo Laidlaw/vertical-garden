024f3afe18f49534399b232c6bd8f19c2bbb3c12
Alan Laidlaw <alan.laidlaw@iostudio.com>
2014-06-05 13:03:28 -0500

    created a static site generator with gulp

--- README.md
+++ README.md
@@ -31,3 +31,7 @@ Forrest has a pretty interesting technique with fittext that we will document so
 Waypoints is a questionable choice because it relies on jquery but it seems all sites these days use scroll/viewport detection. It may be useful in the future to make this a yeoman generator with opinionated options for susy, waypoints and fittext.
 
 Got a crazy idea to include polymer so that I can easily link docssa style components into whatever page I want to make. The test will be whether I can construct polymer components in a similar manner to docssa.
+
+Polymer was overkill for what I was trying to do. I tried out [Metalsmith](http://www.robinthrift.com/posts/getting-to-know-metalsmith/) which is very nice but for some reason does not work well with Gulp. Now I'm trying out [Gulp as a static site generator](https://medium.com/objects-in-space/considering-a-static-site-tool-learn-gulp-2fd5f9821fc4).
+
+Gulp and gulp-file-include seem to be working great. THe next thing to do is structure the partials, templates and content to be similar to docssa. I'd like to try polymer again but logic behind elements is sort at odds with docssa.
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp'),
       sass = require('gulp-ruby-sass'),
+      fileinclude = require('gulp-file-include'),
+      rename = require('gulp-rename'),
+      notify = require('gulp-notify'),
       browserSync = require('browser-sync'),
       plumber = require('gulp-plumber'),
+      autoprefixer = require('gulp-autoprefixer'),
       jshint = require('gulp-jshint'),
-      stylish = require('jshint-stylish');
-
+      stylish = require('jshint-stylish'),
+      path = require("path");
 
 var paths = {
+    partials: '.partials/',
     stylesheets: ['sass/**/*.scss'],
     scripts: ['js/**/*.js']
 };
-
-gulp.task('develop', ['watch', 'browser-sync']);
 
 gulp.task('styles', function () {
     'use strict';
@@ -25,19 +28,40 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function() {
-    'use strict';
-    gulp.watch(paths.stylesheets, ['styles']);
+// fileinclude: grab partials from templates and render out html files
+// ==========================================
+gulp.task('fileinclude', function() {
+  return  gulp.src(path.join(paths.partials, '*.tpl.html'))
+    .pipe(fileinclude())
+    .pipe(rename({
+      extname: ""
+     }))
+    .pipe(rename({
+      extname: ".html"
+     }))
+    .pipe(gulp.dest('./'))
+    // .pipe(livereload(server))
+    .pipe(notify({ message: 'Includes: included' }));
 });
 
+
+
+// browser-sync:
+// ==========================================
 gulp.task('browser-sync', function() {
     'use strict';
-    browserSync.init(['css/**/*.css', '*.html'], {
+    browserSync.init(['css/**/*.css', '*.html', "js/*.js"], {
         server: {
             baseDir: './'
         }
     });
 });
+
+gulp.task('watch', function() {
+    'use strict';
+    gulp.watch(paths.stylesheets, ['styles']);
+});
+
 
 gulp.task('lint-scripts', function() {
     'use strict';
@@ -46,3 +70,8 @@ gulp.task('lint-scripts', function() {
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
+
+// browser-sync:
+// ==========================================
+gulp.task('default', ['fileinclude', 'watch', 'browser-sync']);
+
